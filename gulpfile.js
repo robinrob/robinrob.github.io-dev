29a8b84a84784b6d95102f1fe234147008c53a03
Robin Smith <robin@mrrobinsmith.com>
2015-06-10 15:15:45 +0100

    Restructor

--- gulpfile.js
+++ gulpfile.js
@@ -65,8 +65,8 @@ gulp.task('sass', function () {
 
 gulp.task('haml-watch', function() {
     //var here = path.resolve('./') - destination of '.' in gulp.dest() means relative to src!
-    // Actually, just going to use an index/ folder, solves the problem easily!
-    var locations = ['index', '_includes', '_layouts', 'cv']
+    // Actually, just going to use an index/ and maintenance/ folders, solves the problem easily!
+    var locations = ['index', 'maintenance', '_includes', '_layouts', 'cv']
     locations.forEach(function(location) {
         var src = location + '/haml/*.haml'
         var dest = location
