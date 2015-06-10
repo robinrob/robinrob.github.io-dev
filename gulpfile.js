fb42cff7da33bc83748efc5084e87fec37629ea2
Robin Smith <robin@mrrobinsmith.com>
2015-06-10 12:26:59 +0100

    Finally

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,10 @@ var gcallback   = require('gulp-callback')
 var plumber     = require('gulp-plumber')
 var watch       = require('gulp-watch')
 var changed     = require('gulp-changed')
+var ngmin       = require('gulp-ngmin')
+var task        = require('gulp-task')
+var runsequence        = require('run-sequence')
+//var jekyll      = require('gulp-jekyll')
 require('shelljs/global')
 
 
@@ -18,23 +22,30 @@ var messages = {
 
 function onError(err) {
     console.log(err)
-    exec('say what the fuck')
+    exec("say wanker")
 }
 
-/**
- * Build the Jekyll Site
- */
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-        .on('close', done);
-});
+
+gulp.task('jekyll-build', function(done) {
+    browserSync.notify(messages.jekyllBuild)
+    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'}).on('close', done);
+})
+
+//gulp.task('jekyll-build', function () {
+//    gulp.src(['./index.html', './_layouts/*.html', './_includes/*.html', './_posts/*.{markdown,md}'])
+//        .pipe(jekyll({
+//            source: './',
+//            destination: './_site/',
+//            bundleExec: true
+//        }))
+//        .pipe(gulp.dest('./_site/'));
+//});
 
 /**
  * Rebuild Jekyll & do page reload
  */
 gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
+    reload()
 });
 
 /**
@@ -60,22 +71,63 @@ gulp.task('sass', function () {
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
         .pipe(gulp.dest('css'));
 });
 
 // Watch for changes in Haml files
+//gulp.task('haml-watch', function() {
+//
+//    locations.forEach(function(location) {
+        //var location = '_includes'
+        //var src = location + '/haml/*.haml'
+        //var dest = location
+        //console.log("src: " + src)
+    //return gulp.src(src).
+    //        pipe(plumber({
+    //            onError: onError
+    //        })).
+    //        pipe(watch(src)).
+    //        pipe(changed(dest, {extension: '.html'})).
+    //        pipe(haml()).
+    //        pipe(gulp.dest(dest))
+//})
+
 gulp.task('haml-watch', function() {
-    gulp.src('**/haml/*.haml').
-        pipe(plumber({
-            onError: onError
-        })).
-        pipe(watch('**/haml/*.haml')).
-        pipe(changed('../', {extension: '.html'})).
-        pipe(haml()).
-        pipe(gulp.dest('../')).
-        pipe(gcallback(reload))
+    var locations = ['/Users/msl/Programming/robin/ruby/projects/robinrob.github.io-dev', '_includes', '_layouts', 'cv']
+    locations.forEach(function(location) {
+        var src = location + '/haml/*.haml'
+        var dest = location
+        gulp.src(src).
+            pipe(plumber({
+                onError: onError
+            })).
+            pipe(watch(src)).
+            pipe(changed(dest, {extension: '.html'})).
+            pipe(haml()).
+            pipe(gulp.dest(dest)).
+            pipe(gcallback(function () {
+                console.log("HAML DONE")
+                runsequence('jekyll-rebuild')
+            }))
+    })
 })
+
+// Watch for changes in Haml files
+//gulp.task('haml-watch', function() {
+//    var dest = '.'
+//    gulp.src('./*.haml').
+//        pipe(plumber({
+//            onError: onError
+//        })).
+//        pipe(watch('./*.haml')).
+//        pipe(changed(dest, {extension: '.html'})).
+//        pipe(haml()).
+//        pipe(gulp.dest(dest)).
+//        pipe(gcallback(reload))
+//})
+
+
+// gulp.task('reload', function() {
 
 
 /**
@@ -83,7 +135,7 @@ gulp.task('haml-watch', function() {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch(['_posts/*', 'css/*', '_sass/*', 'img/*', 'js/*', 'orbiter/**/*'], ['jekyll-rebuild']);
+    gulp.watch(['_posts/*', 'img/*', '_sass/*.scss', 'orbiter/**/*'], ['jekyll-rebuild']);
 });
 
 /**
