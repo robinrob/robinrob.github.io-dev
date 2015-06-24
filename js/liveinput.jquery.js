e21226c3d7cecfba70039e4e019494adf955f6e2
Robin Smith <robin@mrrobinsmith.com>
2015-06-24 02:58:44 +0100

    Revert "Aligned headings"
    This reverts commit 172231b03f27ad8bf4d459e3bef6a7b290fab1cf.

--- js/liveinput.jquery.js
+++ js/liveinput.jquery.js
@@ -39,18 +39,8 @@
                 html: char,
                 class: "char"
             });
-            $char.insertBefore($cursorObj)
+            $($char).insertBefore($cursorObj)
             keyPress()
-        }
-
-        function writeInvisibleCursor($cursorObj) {
-            var $char = $("<span />", {
-                html: "_"
-            });
-            $char.css({
-                "opacity": 0
-            })
-            $char.insertBefore($cursorObj)
         }
 
         function writeText(text, $cursorObj, callback) {
@@ -87,15 +77,11 @@
                 text: "_",
                 class: "cursor"
             });
-            $cursor.css({
-                "z-index" : "1",
-                "margin-left": "0.05em"
-            })
+            $cursor.css("margin-left", "0.05em")
 
             setTimeout(function () {
                 reset()
                 $this.append($cursor)
-                writeInvisibleCursor($cursor)
                 writeText(params.text, $cursor, function() {
                     fadeOutCursor()
                     params.callback()
