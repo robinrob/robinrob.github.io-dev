ba08fc26fc9855a88b0a02b8a4a928f0b51ba0d3
robinrob <robin@mrrobinsmith.com>
2015-08-29 13:12:21 +0100

    Update 2015-08-29T12:11:11.529Z

--- _secret-config.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = function(config) {
-    return require('merge')(config, {
-        cloudflare: {
-            token  : '431609236d8480cf29f6cfbc2cbe34374ced5',
-            email  : 'rs@rsmith.io',
-            domain : 'rsmith.io'
-        },
-    })
-}
